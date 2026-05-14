65c0e0315002c56d9c6fa0bae96c5996466e817b
mohammed alsalim <mo.as.kb-2010@hotamil.com>
2026-05-14 21:33:22 +0300

    update feign client

--- Order-server/src/main/java/com/example/orderserver/client/CartClient.java
+++ Order-server/src/main/java/com/example/orderserver/client/CartClient.java
@@ -4,12 +4,12 @@ import com.example.orderserver.data.dto.CartDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "Cart-server", url = "http://localhost:8081/api/cart") // عدّل الـ URL حسب خدمة الكارت
+// 👈 التعديل هنا: اسم الخدمة بأحرف صغيرة، وإضافة path لتعويض مسار الـ API
+@FeignClient(name = "cart-server", path = "/api/cart")
 public interface CartClient {
 
-    @PostMapping ("/get-or-create")
+    @PostMapping("/get-or-create")
     CartDto getCart(
-
             @RequestHeader(value = "cartId", required = false) String cartId,
             @RequestHeader(value = "Authorization", required = false) String authorization
     );
